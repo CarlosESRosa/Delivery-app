28936ff3e2785c08ccb8e2d2df21ec6cf0ab3bb0
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-08-04 17:27:04 -0300

    feat: add migration create-sale

---
+++ back-end/src/database/migrations/20220804200524-create-sale.js
@@ -0,0 +1,53 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('sales', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        allowNull: false,
+        field: 'user_id',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        type: Sequelize.INTEGER
+      },
+      seller_id: {
+        allowNull: false,
+        field: 'seller_id',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        type: Sequelize.INTEGER
+      },
+      total_price: {
+        type: Sequelize.DECIMAL(9, 2)
+      },
+      delivery_address: {
+        type: Sequelize.STRING(100)
+      },
+      delivery_number: {
+        type: Sequelize.STRING(50)
+      },
+      sale_date: {
+        type: Sequelize.DATE
+      },
+      status: {
+        type: Sequelize.STRING(50)
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('sales');
+  }
+};
