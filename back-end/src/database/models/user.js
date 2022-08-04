85517768a5205aa8e0a63c70280e80331dd8d4ba
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-08-04 16:41:12 -0300

    feat: add user model

---
+++ back-end/src/database/models/user.js
@@ -0,0 +1,15 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { autoIncrement: true, primaryKey: true,  type: DataTypes.INTEGER },
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    role: DataTypes.STRING
+  }, {
+    timestamps: false 
+  });
+
+  return User;
+}
+
+export default User;
