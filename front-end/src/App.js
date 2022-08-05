5814f5c6611ad62c5a0612c49ebfc0d5a533fede
thinito <thiagogomez2906@hotmail.com>
2022-08-05 13:56:11 -0300

    fix lint

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes as Switch, Route } from 'react-router-dom';
 import Register from './pages/Register';
-import Products from './pages/Products'
+import Products from './pages/Products';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/register" element={ <Register /> } />
-        <Route exact path="/customer/products" element={<Products/>}/>
+        <Route exact path="/customer/products" element={ <Products /> } />
       </Switch>
     </Router>
   );
--- front-end/src/pages/Products.js
+++ front-end/src/pages/Products.js
@@ -1,26 +1,30 @@
-import React, { useState } from 'react';
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Product from '../components/Product';
+
 export default function Products() {
-    const products = [];
-    //implementar o banco de dados
-    return(
+  const products = [];
+  return (
     <div>
-        <nav>
-            <button type='button' onClick={ useNavigate("/customer/products")}>
-            PRODUTOS</button>
-            <button type='button' onClick={ useNavigate("/customer/orders")}>
-            MEUS PEDIDOS</button>
-            <button type='button'>
-            <USER/></button>
-            <button type='button'onClick={ useNavigate("/")}>
-            Sair</button>
-        </nav>
-        <body>
-            {products.forEach(p => {
-            <Product props={p}/>
-            })}
-        </body>
+      <nav>
+        <button type="button" onClick={ useNavigate('/customer/products') }>
+          PRODUTOS
+        </button>
+        <button type="button" onClick={ useNavigate('/customer/orders') }>
+          MEUS PEDIDOS
+        </button>
+        <button type="button">
+          user
+        </button>
+        <button type="button" onClick={ useNavigate('/') }>
+          Sair
+        </button>
+      </nav>
+      <body>
+        {products.forEach((p) => {
+          <Product props={ p } />;
+        })}
+      </body>
     </div>
-    )
+  );
 }
