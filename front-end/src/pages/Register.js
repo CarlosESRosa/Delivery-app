ad3812f609206aa6b49807c7ed0df80889450d2e
Felipe Castanheira <castanheirafc2@gmail.com>
2022-08-05 08:44:07 -0300

    button validation

--- front-end/src/pages/Register.js
+++ front-end/src/pages/Register.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isEmailValid, setIsEmailValid] = useState(false);
+  const handleEmail = ({ target }) => {
+    setEmail(target.value);
+    setIsEmailValid(target.value.includes('.') && target.value.includes('@'));
+  };
+  const nMinLength = 12;
+  const pMinLength = 6;
   return (
     <form>
       <h1>Cadastro</h1>
@@ -9,8 +19,10 @@ function Register() {
         <input
           id="common_register__input-name"
           data-testid="common_register__input-name"
+          value={ name }
           placeholder="Seu nome"
           type="text"
+          onChange={ ({ target }) => setName(target.value) }
         />
       </label>
       <label htmlFor="common_register__input-email">
@@ -18,8 +30,10 @@ function Register() {
         <input
           id="common_register__input-email"
           data-testid="common_register__input-email"
+          value={ email }
           placeholder="Email"
           type="text"
+          onChange={ handleEmail }
         />
       </label>
       <label htmlFor="common_register__input-password">
@@ -27,11 +41,19 @@ function Register() {
         <input
           id="common_register__input-password"
           data-testid="common_register__input-password"
+          value={ password }
           placeholder="Senha"
           type="password"
+          onChange={ ({ target }) => setPassword(target.value) }
         />
       </label>
-      <button type="button" data-testid="common_register__button-register">
+      <button
+        type="button"
+        data-testid="common_register__button-register"
+        disabled={
+          name.length < nMinLength || !isEmailValid || password.length < pMinLength
+        }
+      >
         Cadastrar
       </button>
       <span data-testid="common_register__element-invalid_register" />
