6b8869e928c2c627918fae70e3c78f5eb440d711
Carlos Eduardo <carlosrosa1001@gmail.com>
2022-08-05 10:27:10 -0300

    Req 1 - Rota para pagina de login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes as Switch, Route } from 'react-router-dom';
 import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route exact path="/" element={ <Login /> } />
+        <Route exact path="/login" element={ <Login /> } />
         <Route exact path="/register" element={ <Register /> } />
       </Switch>
     </Router>
