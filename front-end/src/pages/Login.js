0e617f2462a71488054f036d0f71e02390b8dc6b
Felipe Castanheira <castanheirafc2@gmail.com>
2022-08-05 19:48:40 -0300

    register navigation

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
   const handleChange = ({ target }, setter) => {
     setter(target.value);
@@ -12,20 +14,26 @@ function Login() {
     <form>
       <input
         type="email"
-        data-testid="1"
+        data-testid="common_login__input-email"
         placeholder="email@trybeer.com.br"
         value={ email }
         onChange={ (event) => handleChange(event, setEmail) }
       />
       <input
         type="password"
-        data-testid="1"
+        data-testid="common_login__input-password"
         placeholder="********"
         value={ password }
         onChange={ (event) => handleChange(event, setPassword) }
       />
-      <button type="button" data-testid="3">LOGIN</button>
-      <button type="button" data-testid="4">Ainda não tenho uma conta</button>
+      <button type="button" data-testid="common_login__button-login">LOGIN</button>
+      <button
+        type="button"
+        data-testid="common_login__button-register"
+        onClick={ () => navigate('./register') }
+      >
+        Ainda não tenho uma conta
+      </button>
     </form>
   );
 }
