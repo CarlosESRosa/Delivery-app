1393b52366e1f74b6f16336a4dbc6d1fc2d4b867
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-08-08 18:47:06 -0300

    feat: add create and find methods

---
+++ back-end/src/api/controllers/sale.controller.js
@@ -0,0 +1,33 @@
+const Sale = require('../services/sale.service');
+
+const create = async (req, res, next) => {
+  const { user, body } = req;
+
+  body.userId = user.id;
+
+  const sale = await Sale.create(body);
+
+  if (sale.error) return next(sale.error);
+  
+  return res.status(201).json(sale);
+};
+
+const findAll = async (req, res) => {
+  const { user } = req;
+
+  const data = await Sale.findAll(user.id, user.role);
+
+  return res.status(200).json(data);
+};
+
+const findById = async (req, res, next) => {
+  const { user, params: { id } } = req;
+
+  const data = await Sale.findById(user.id, user.role, id);
+
+  if (data.error) return next(data.error);
+
+  return res.status(200).json(data);
+};
+
+module.exports = { create, findAll, findById };
