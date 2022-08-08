036c5ab6b3c391e4f567488c7012817a1ae19e88
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-08-08 17:19:21 -0300

    fix: change-clomun-status migration

--- back-end/src/database/migrations/20220808175627-change-column-status-table-sales.js
+++ back-end/src/database/migrations/20220808175627-change-column-status-table-sales.js
@@ -1,18 +1,14 @@
-'use strict';
-
-const { DataTypes } = require("sequelize/types");
-
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.changeColumn('users', 'status', {
-      type: DataTypes.STRING(50),
+      type: Sequelize.STRING(50),
       defaultValue: 'Pendente'
     });
   },
 
   async down (queryInterface, Sequelize) {
     await queryInterface.changeColumn('users', 'status', {
-      type: DataTypes.STRING(50),
+      type: Sequelize.STRING(50),
     });
   }
 };
