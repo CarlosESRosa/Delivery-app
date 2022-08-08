cc541d1eda55a4a061e5cf46af003c7d1b14ea09
Carlos Eduardo <carlosrosa1001@gmail.com>
2022-08-08 14:45:37 -0300

    feat: :sparkles: add cors to project

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = require('./routes');
 const errorMiddleware = require('./middlewares/errorMiddleware');
-
+var cors = require('cors')
 const app = express();
 
 app.use(express.json());
+
+app.use(cors())
 
 app.use('/', router);
 
