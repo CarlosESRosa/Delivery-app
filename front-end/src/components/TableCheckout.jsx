9da28bf59dc12da4b98a5b0091224fb5115e8f20
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-08-12 10:13:31 -0300

    fix: req.17

--- front-end/src/components/TableCheckout.jsx
+++ front-end/src/components/TableCheckout.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import formatCurrency from '../helpers/formatCurrency';
 
-export default function TableCheckout({ name, price, value, index }) {
+export default function TableCheckout({ id, name, price, value, index, removerProduto }) {
   return (
     <tr>
       <td data-testid={ `customer_checkout__element-order-table-item-number-${index}` }>
@@ -14,15 +15,16 @@ export default function TableCheckout({ name, price, value, index }) {
         {value}
       </td>
       <td data-testid={ `customer_checkout__element-order-table-unit-price-${index}` }>
-        {price.toFixed(2)}
+        {formatCurrency(price.toFixed(2))}
       </td>
       <td data-testid={ `customer_checkout__element-order-table-sub-total-${index}` }>
-        {price * value.toFixed(2)}
+        {formatCurrency(price * value)}
       </td>
       <td>
         <button
           type="button"
           data-testid={ `customer_checkout__element-order-table-remove-${index}` }
+          onClick={ () => removerProduto(id) }
         >
           Remover
         </button>
@@ -33,7 +35,9 @@ export default function TableCheckout({ name, price, value, index }) {
 
 TableCheckout.propTypes = {
   id: PropTypes.number,
+  index: PropTypes.number,
   name: PropTypes.string,
   price: PropTypes.number,
   value: PropTypes.number,
+  removerProduto: PropTypes.func,
 }.isRequired;
--- front-end/src/pages/Checkout.js
+++ front-end/src/pages/Checkout.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import TableCheckout from '../components/TableCheckout';
-import { getCart } from '../helpers/localStore';
+import { getSellers, getUser, saveSale } from '../helpers/fetchAPI';
+import formatCurrency from '../helpers/formatCurrency';
+import { getCart, getLocalUser, removeItem } from '../helpers/localStore';
 
 export default function Checkout() {
-  const cart = getCart();
+  const [cart, setCart] = useState(getCart());
+  const [address, setAddress] = useState();
+  const [number, setNumber] = useState();
+  const [sellers, setSellers] = useState([]);
+  const [seller, setSeller] = useState();
+
+  const getAllSellers = async () => {
+    const res = await getSellers();
+    setSellers(res.data);
+  };
+
+  const handleSubmit = async () => {
+    const user = await getUser(getLocalUser().token);
+
+    const sale = {
+      deliveryAddress: address,
+      deliveryNumber: number,
+      sellerId: seller,
+      userId: user.id,
+      totalPrice: cart.total.toFixed(2),
+      products: getCart().items,
+    };
+
+    await saveSale(getLocalUser().token, sale);
+  };
+
+  useEffect(() => getAllSellers());
+
+  const removeProduct = (id) => {
+    removeItem(id);
+    setCart(getCart());
+  };
+
   return (
     <div>
       <Header />
@@ -19,29 +53,43 @@ export default function Checkout() {
             <td>Sub-total</td>
             <td>Remover item</td>
           </tr>
-          {cart.items ? cart.items.map(({ name, price, value }, index) => (
+          {cart.items ? cart.items.map(({ id, name, price, value }, index) => (
             <TableCheckout
               key={ index }
+              id={ id }
               index={ index }
               name={ name }
               price={ price }
               value={ value }
+              removeProduct={ removeProduct }
             />
           )) : 'test'}
         </table>
         <aside data-testid="customer_checkout__element-order-total-price">
-          Total:R$
-          {cart.total.toFixed(2)}
+          Total:
+          {formatCurrency(cart.total.toFixed(2))}
         </aside>
         <h4>Detalhes de Endereço para Entrega</h4>
         <form>
-
-          {/*
-        <label htmlFor="">
-          P. vendedora responsavel:
-          <input />
-        </label>
-        */}
+          <label htmlFor="select-seller">
+            P. vendedora responsavel:
+            <select
+              data-testid="customer_checkout__select-seller"
+              id="select-seller"
+              onChange={ (e) => setSeller(e.target.value) }
+              value={ seller }
+            >
+              {
+                sellers.length > 0 && (
+                  sellers.map((sellerData) => (
+                    <option key={ `seller-${sellerData.id}` } value={ sellerData.id }>
+                      {sellerData.name}
+                    </option>
+                  ))
+                )
+              }
+            </select>
+          </label>
           <label htmlFor="endereco">
             Endereço
             <input
@@ -49,6 +97,8 @@ export default function Checkout() {
               name="endereco"
               data-testid="customer_checkout__input-address"
               placeholder="Travessa Terceira da Castanheira, Bairro Muruci"
+              onChange={ (e) => setAddress(e.target.value) }
+              value={ address }
             />
           </label>
           <label htmlFor="numero">
@@ -58,11 +108,14 @@ export default function Checkout() {
               name="numero"
               data-testid="customer_checkout__input-addressNumber"
               placeholder="198"
+              onChange={ (e) => setNumber(e.target.value) }
+              value={ number }
             />
           </label>
           <button
             type="button"
             data-testid="customer_checkout__button-submit-order"
+            onClick={ handleSubmit }
           >
             FINALIZAR PEDIDO
           </button>
