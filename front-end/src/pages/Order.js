1ba55ad196964f4b0eb1436ce51ee88c5305c90e
Carlos Eduardo <carlosrosa1001@gmail.com>
2022-08-16 18:42:58 -0300

    commit test

--- front-end/src/pages/Order.js
+++ front-end/src/pages/Order.js
@@ -20,7 +20,6 @@ function Order() {
 
   useEffect(() => getData(), []);
   useEffect(() => getData(), [saleData]);
-  // commit teste
   const LABEL_OR_TEST_ID = 'customer_order_details__element-order-details-label-order-id';
   const LABEL_NAME = 'customer_order_details__element-order-details-label-seller-name';
   const LABEL_DATE = 'customer_order_details__element-order-details-label-order-date';
